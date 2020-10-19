7cfd6f0f3e8c174f204e0c10b342546b113176a8
renaldysab18 <renaldysab18@gmail.com>
2020-10-20 00:24:40 +0700

    [add] adding get data game

--- features/home/src/main/java/com/redveloper/home/ui/HomeFragment.kt
+++ features/home/src/main/java/com/redveloper/home/ui/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.redveloper.home.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import com.redveloper.core.vo.Resource
 import com.redveloper.home.R
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
+
+    val homeViewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,4 +23,24 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (activity != null){
+            homeViewModel.games().observe(viewLifecycleOwner, Observer { data ->
+                if (data != null){
+                    when(data) {
+                        is Resource.Loading -> {
+
+                        }
+                        is Resource.Success -> {
+                            Log.i("dataGame", data.data.toString())
+                        }
+                        is Resource.Error -> {
+                            Log.i("dataGame", data.message.toString())
+                        }
+                    }
+                }
+            })
+        }
+    }
 }
--- features/home/src/main/java/com/redveloper/home/ui/HomeViewModel.kt
+++ features/home/src/main/java/com/redveloper/home/ui/HomeViewModel.kt
@@ -1,11 +1,12 @@
 package com.redveloper.home.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.redveloper.home.core.domain.usecase.HomeUseCase
 
 class HomeViewModel (
     private val useCase: HomeUseCase
 ) : ViewModel() {
 
-    fun games() = useCase.getAllGame()
+    fun games() = useCase.getAllGame().asLiveData()
 }
