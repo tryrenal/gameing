1c936bafc9783e49cfb74f1acb34ab339bbccb0d
renaldysab18 <renaldysab18@gmail.com>
2020-10-19 19:29:54 +0700

    [add] adding api response and remote data source

--- core/src/main/java/com/redveloper/core/data/source/remote/RemoteDataSource.kt
+++ core/src/main/java/com/redveloper/core/data/source/remote/RemoteDataSource.kt
@@ -1,2 +1,28 @@
 package com.redveloper.core.data.source.remote
 
+import com.redveloper.core.data.source.remote.network.ApiService
+import com.redveloper.core.data.source.remote.response.game.GameResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class RemoteDataSource(private val apiService: ApiService) {
+
+    fun getAllGames(): Flow<ApiResponse<List<GameResponse>>> {
+        return flow {
+            try {
+                val response = apiService.getAllGames()
+                val dataArray = response.results
+                if (dataArray.isNotEmpty()) {
+                    emit(ApiResponse.Success(response.results))
+                } else {
+                    emit(ApiResponse.Empty)
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
