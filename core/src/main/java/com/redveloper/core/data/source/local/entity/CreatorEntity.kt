3ab3e91ee1bde4e437ce78f21a77f42ec503039f
renaldysab18 <renaldysab18@gmail.com>
2020-10-20 18:58:37 +0700

    [add] adding creator entity

---
+++ core/src/main/java/com/redveloper/core/data/source/local/entity/CreatorEntity.kt
@@ -0,0 +1,22 @@
+package com.redveloper.core.data.source.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class CreatorEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "slug")
+    val slug: String?,
+    @ColumnInfo(name = "image")
+    val image: String?,
+    @ColumnInfo(name = "imageBackground")
+    val imageBackgroudn: String?,
+    @ColumnInfo(name = "gamesCount")
+    val gamesCount: String?
+)
