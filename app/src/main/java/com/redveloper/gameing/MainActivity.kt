547dcd3db29fe6e0ff4f8dafe0e10f462744712e
renaldysab18 <renaldysab18@gmail.com>
2020-10-25 09:27:17 +0700

    [add] adding navigation cross modul

--- app/src/main/java/com/redveloper/gameing/MainActivity.kt
+++ app/src/main/java/com/redveloper/gameing/MainActivity.kt
@@ -2,22 +2,26 @@ package com.redveloper.gameing
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.redveloper.creator.ui.CreatorFragment
-import com.redveloper.home.ui.HomeFragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        moveTofragment(CreatorFragment())
+        navView = findViewById(R.id.bottom_nav_main)
+        navController = findNavController(R.id.nav_host_fragment)
+
+        navView.setupWithNavController(navController)
+
     }
 
-    fun moveTofragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, fragment)
-            .commit()
-    }
+
 }
